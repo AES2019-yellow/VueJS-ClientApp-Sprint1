97949c76455981e2a9beb3128215d8da44a9a2d2
Dorado Andres <andres.dorado90@gmail.com>
2019-11-27 17:09:01 +0100

    minor fix

--- src/store.js
+++ src/store.js
@@ -6,7 +6,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state:{
-        token: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywidXNlcm5hbWUiOiJ1c2VyMSIsImVtYWlsIjoidXNlcjFAZXhhbXBsZS5jb20iLCJpYXQiOjE1NzQ1MjI2ODksImV4cCI6MTU3NDU0NDI4OX0.gKedQiHONGrppjQEf988Jrce7IomSQsdsS-DHzTecic",
+        token: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjo0LCJ1c2VybmFtZSI6InRlc3QiLCJmaXJzdG5hbWUiOiJ0ZXN0IiwibGFzdG5hbWUiOiJ0ZXN0IiwiZW1haWwiOiJ0ZXN0QGV4YW1wbGUuY29tIn0sImlhdCI6MTU3NDg0ODM3NH0.MlxgFp-EB2TYg7qnPx0LJjIdg9m8nOztMk2yHz_VO28",
 
     },
     mutations: {
